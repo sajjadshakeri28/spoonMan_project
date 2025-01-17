931d47d6ca593bc414ae3fc3408487310b5be53f
sajjadshakeri <sajjadwhat85@gmail.com>
2025-01-17 17:14:13 +0330

    include and color,gotoxy function aded

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main()
-{
-    cout << "hello world!" ;
-
-    return 0;
+void setColor(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
 }
+
+void gotoXY(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
