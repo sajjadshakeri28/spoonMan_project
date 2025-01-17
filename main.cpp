a790ad44020eeaba313a83521920c72328341bb0
sajjadshakeri <sajjadwhat85@gmail.com>
2025-01-17 17:20:17 +0330

    placing player, concerate walls

--- main.cpp
+++ main.cpp
@@ -17,3 +17,31 @@ void gotoXY(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+int main() {
+    srand(time(0));
+
+    gotoXY(30, 1);
+    cout << "Hello to game" << endl << endl;
+
+    gotoXY(3, 5);
+    setColor(10);
+    cout << "SS";
+
+    for (int i = 9; i < 60; i += 6) {
+        for (int j = 5; j < 20; j += 4) {
+            setColor(8);
+            gotoXY(i, j);
+            cout << "XX";
+        }
+    }
+
+    for (int i = 5; i < 60; i += 6) {
+        for (int j = 5; j < 20; j += 2) {
+            setColor(15);
+            gotoXY(i, j);
+            cout << "|  |";
+        }
+    }
+
+   
+
